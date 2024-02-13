e535f34af22dc06433e6cf9485dde98e203fc3b4
Alireza <a.razinejad@gmail.com>
2024-02-13 12:24:39 +0330

    feat: implement the virtual scroll solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import names from './names.json';
+import { useVirtualizer } from '@tanstack/react-virtual';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function NameList() {
+    const parentRef = React.useRef();
+    const [showAllNames, setShowAllNames] = useState(false);
+
+    const rowVirtualizer = useVirtualizer({
+        count: showAllNames ? names.length : 100,
+        getScrollElement: () => parentRef.current,
+        estimateSize: () => 25,
+
+    });
+
+    return (
+        <div>
+            <button
+                style={{
+                    padding: '10px',
+                    backgroundColor: 'skyblue',
+                    outline: 'none',
+                    border: '1px solid white',
+                    borderRadius: '5px',
+                    margin: '1rem'
+                }}
+                onClick={() => {
+                    setShowAllNames(!showAllNames);
+                }}
+            >
+                {showAllNames ? 'Hide All Names' : 'Show All Names'}
+            </button>
+            <div
+                ref={parentRef}
+                style={{
+                    height: `400px`,
+                    overflow: 'auto', // Make it scroll!
+                    border: '1px solid black',
+                    padding: '1rem',
+                    borderRadius: '1rem',
+                    marginInline: '1rem',
+                    marginTop: '1rem'
+                }}
+            >
+                <div
+                    style={{
+                        height: `${rowVirtualizer.getTotalSize()}px`,
+                        width: '100%',
+                        position: 'relative',
+                    }}
+                >
+                    {rowVirtualizer.getVirtualItems().map((virtualItem) => (
+                        <div
+                            key={virtualItem.key}
+                            style={{
+                                position: 'absolute',
+                                top: 0,
+                                left: 0,
+                                width: '100%',
+                                height: `${virtualItem.size}px`,
+                                transform: `translateY(${virtualItem.start}px)`,
+                            }}
+                        >
+                            ({virtualItem.index + 1}). First Name: {names[virtualItem.index].first}
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default App;
+export default NameList;
